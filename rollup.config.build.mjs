f7bba81d6cdda3e59f8fb6f9cca81520d839fdf5
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-04-15 00:38:14 +0200

    feat: ✨ new npm script add-module-type.js

--- rollup.config.build.mjs
+++ rollup.config.build.mjs
@@ -3,7 +3,6 @@ import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import {glob} from 'glob';
 import json from "@rollup/plugin-json";
-import fs from 'fs-extra'; // Importuj fs-extra do manipulacji plikami
 import babel from "@rollup/plugin-babel";
 
 const files = glob.sync('src/**/*.ts', { nodir: true });
@@ -39,17 +38,9 @@ export default {
         typescript({
             tsconfig: 'tsconfig.json',
         }),
-        {
-            name: 'add-module-type',
-            generateBundle() {
-                // Dodaj pole "type": "module" do package.json po wygenerowaniu plików
-                const packageJsonPath = 'dist/package/package.json';
-                const packageJson = fs.readJsonSync(packageJsonPath);
-                packageJson.type = 'module';
-                fs.writeJsonSync(packageJsonPath, packageJson, { spaces: 2 });
-            },
-        },
+
     ],
+
 
 };
 
