47d1254c9365399310b612adbc2cc5a555313f0b
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-04-15 00:49:47 +0200

    feat: ✨ change wget to import

--- src/util/wget.ts
+++ src/util/wget.ts
@@ -1,4 +1,5 @@
-const wget = require('node-wget');
+import wget from 'node-wget';
+
 export const wgetAsync = (url: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     wget({ url }, (err: any, response: any, content: string | PromiseLike<string>) => {
