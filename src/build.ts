465875f0f0db3e346b271b51bcf0b76a1365383a
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-05-17 00:22:11 +0200

    feat: ✨ build from snp files

--- src/build.ts
+++ src/build.ts
@@ -2,10 +2,12 @@
 
 import minimist from 'minimist';
 import { Args } from '@/feature/args';
-import { ConfigType, getConfig } from '@/feature/defaultConfig';
+import { ConfigType, createPath, getConfig } from '@/feature/defaultConfig';
 import { createCatalog } from '@/util/createCatalog';
+import { createFile } from '@/util/createFile';
 import { debugFunction } from '@/util/debugFunction';
 import { deleteCatalog } from '@/util/deleteCatalog';
+import { readFile } from '@/util/readFile';
 
 export const build = async (args: Args): Promise<ConfigType> => {
   const config = await getConfig(args);
@@ -18,12 +20,38 @@ export const build = async (args: Args): Promise<ConfigType> => {
 };
 
 export const buildSNP = async (config: ConfigType): Promise<ConfigType> => {
-  // budowanie z plikow
-  // default
-  // extend
-  // custom
-  //
+  // if (config && config.fileMap && config.fileMap.snpFiles) {
+  const snpFiles = config.fileMap?.snpFiles;
+  for (const fileName in snpFiles) {
+    if (snpFiles.hasOwnProperty(fileName)) {
+      const snpFile = snpFiles[fileName];
 
+      const snpTypeFile = ['defaultFile', 'instructionsFile', 'customFile', 'extendFile'];
+      const contentFile = {};
+      for (const fileType of snpTypeFile) {
+        contentFile[fileType] = await readFile(snpFile[fileType]);
+      }
+
+      const getContnetToBuild = () => {
+        if (contentFile['customFile']) {
+          return contentFile['customFile'];
+        }
+
+        if (contentFile['extendFile']) {
+          return contentFile['defaultFile'] + '\n' + contentFile['extendFile'];
+        }
+
+        return contentFile['defaultFile'];
+      };
+
+      await createFile({
+        filePath: createPath([config.projectCatalog, fileName]),
+        content: getContnetToBuild(),
+        isDebug: config.isDebug,
+      });
+      console.log(getContnetToBuild());
+    }
+  }
   return config;
 };
 
