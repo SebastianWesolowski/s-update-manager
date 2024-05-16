337babef6cb875f56bb33e539ca690467ca141a4
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-05-16 23:58:17 +0200

    refactor: ♻️ config mapFile
    Signed-off-by: Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>

--- src/build.ts
+++ src/build.ts
@@ -2,68 +2,32 @@
 
 import minimist from 'minimist';
 import { Args } from '@/feature/args';
-import { DefaultConfigType, getDefaultConfig } from '@/feature/defaultConfig';
+import { ConfigType, getConfig } from '@/feature/defaultConfig';
+import { createCatalog } from '@/util/createCatalog';
 import { debugFunction } from '@/util/debugFunction';
 import { deleteCatalog } from '@/util/deleteCatalog';
 
-export const build = async (args: Args): Promise<DefaultConfigType> => {
-  const config = await getDefaultConfig(args);
+export const build = async (args: Args): Promise<ConfigType> => {
+  const config = await getConfig(args);
 
   debugFunction(config.isDebug, '=== Start SNP BUILD ===');
 
-  return { ...config };
+  return await createCatalog(config.temporaryFolder).then(() => {
+    return { ...config };
+  });
 };
 
-export const buildSNP = async (config: DefaultConfigType): Promise<DefaultConfigType> => {
-  console.log(config);
-
+export const buildSNP = async (config: ConfigType): Promise<ConfigType> => {
   // budowanie z plikow
   // default
   // extend
   // custom
   //
 
-  //   const { fileMap, ...newConfigContent } = { ...config };
-  //   await updateJson({ filePath: config.snpConfigFile, newContent: { ...newConfigContent } });
-  //   const buildFile = async ({
-  //     fileMap,
-  //   }: {
-  //     fileMap: {
-  //       fileMap: string[];
-  //       files: Record<string, string[]>;
-  //     };
-  //   }) => {
-  //     const files = fileMap.files;
-  //     const contentFile = Object.keys(files);
-  //
-  //     for (const fileName of contentFile) {
-  //       const defaultFile = files[fileName].find((element) => element.includes('-default.md')) || '';
-  //       const customFile = defaultFile.replace('-default.md', '-custom.md');
-  //       const extendFile = defaultFile.replace('-default.md', '-extend.md');
-  //       const contentFile = await readFile(defaultFile);
-  //       await createFile({
-  //         filePath: path.join(config.projectCatalog, fileName),
-  //         content: contentFile,
-  //         isDebug: config.isDebug,
-  //       });
-  //
-  //       await createFile({
-  //         filePath: customFile,
-  //         content: '',
-  //         isDebug: config.isDebug,
-  //       });
-  //       await createFile({
-  //         filePath: extendFile,
-  //         content: '',
-  //         isDebug: config.isDebug,
-  //       });
-  //     }
-  //   };
-  //   await buildFile(config);
   return config;
 };
 
-export const cleanUp = async (config: DefaultConfigType): Promise<any> => {
+export const cleanUp = async (config: ConfigType): Promise<any> => {
   debugFunction(config.isDebug, { config }, 'final config');
   await deleteCatalog(config.temporaryFolder, config.isDebug);
 
