35a9f796e03e42aa73925f700655eaf927295c00
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-08-13 22:57:43 +0200

    feat: ✨ prepare template

---
+++ src/feature/args/argsTemplate.ts
@@ -0,0 +1,25 @@
+export interface ArgsTemplate {
+  projectCatalog?: string;
+  isDebug?: string;
+  _: string[];
+}
+
+export const setArgsTemplate = (args: ArgsTemplate): ArgsTemplate => {
+  if (process.env.SDEBUG === 'true') {
+    args = {
+      _: [],
+      isDebug: 'true',
+      projectCatalog: './template/node',
+    };
+    console.log({ setArgsTemplate: args });
+  }
+
+  const argProjectCatalog = args.projectCatalog || args._[0];
+  const argIsDebug: string = args.isDebug || args._[1];
+
+  return {
+    projectCatalog: argProjectCatalog,
+    isDebug: argIsDebug,
+    _: args._,
+  };
+};
