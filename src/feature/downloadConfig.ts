cc8dae67facd83fd51ab9011cf37f8e0061adc97
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-08-26 23:01:33 +0200

    test: 🚨 downloadConfig

--- src/feature/downloadConfig.ts
+++ src/feature/downloadConfig.ts
@@ -9,7 +9,10 @@ import { parseJSON } from '@/util/parseJSON';
 import { readFile } from '@/util/readFile';
 import { wgetAsync } from '@/util/wget';
 
-export async function downloadConfig(config: ConfigType): Promise<ConfigType> {
+export async function downloadConfig(config: ConfigType): Promise<{
+  downloadContent: FileMapConfig;
+  config: ConfigType;
+}> {
   debugFunction(config.isDebug, '=== SNP INIT ===', '[INIT] downloadConfig');
   const snpFileMapConfig: FileMapConfig = await readFile(config.snpFileMapConfig).then(async (bufferData) =>
     parseJSON(bufferData.toString())
@@ -58,15 +61,15 @@ export async function downloadConfig(config: ConfigType): Promise<ConfigType> {
             overwriteFile: true,
           },
         });
-        return combinedConfig;
+        return { combinedConfig, downloadContent };
       })
-      .then((combinedConfig) => {
+      .then(({ combinedConfig, downloadContent }) => {
         debugFunction(
           config.isDebug,
           { ...config, templateVersion: combinedConfig.templateVersion },
           '[INIT] downloadConfig - end'
         );
-        return { ...config, templateVersion: combinedConfig.templateVersion };
+        return { config: { ...config, templateVersion: combinedConfig.templateVersion }, downloadContent };
       });
   } catch (err) {
     console.error('Error while downloading config from github', err);
