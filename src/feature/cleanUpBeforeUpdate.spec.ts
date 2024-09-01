1ba54a3f8373bdd4078122ea84974db478829172
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-09-01 17:33:09 +0200

    test: 🚨 cleanUpBeforeUpdate extra files
    Signed-off-by: Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>

--- src/feature/cleanUpBeforeUpdate.spec.ts
+++ src/feature/cleanUpBeforeUpdate.spec.ts
@@ -1,5 +1,6 @@
 import { cleanUpFiles } from '@/feature/__tests__/cleanForTests';
 import { mockConfig, mockSnpFileMapConfig } from '@/feature/__tests__/const';
+import { extractAndReplacePaths } from '@/feature/__tests__/extractAndReplacePaths';
 import { searchFilesInDirectory } from '@/feature/__tests__/searchFilesInDirectory';
 import { updateConfigBasedOnComparison } from '@/feature/__tests__/updateConfigBasedOnComparison';
 import { cleanUpBeforeUpdate } from '@/feature/cleanUpBeforeUpdate';
@@ -95,79 +96,79 @@ describe('cleanUpBeforeUpdate', () => {
     });
   });
   //
-  // it('should handle cases where there are no files to clean up', async () => {
-  //   // Set up the mock configuration with no fileMap
-  //   config = { ...mockConfig.step.createConfigFile };
-  //
-  //   await createFile({
-  //     filePath: config.snpConfigFile,
-  //     content: JSON.stringify(config),
-  //   });
-  //   await createFile({
-  //     filePath: config.snpFileMapConfig,
-  //     content: JSON.stringify({
-  //       ...mockSnpFileMapConfig.step.prepareBaseSnpFileMap,
-  //       fileMap: [], // Ensure fileMap is empty
-  //     }),
-  //   });
-  //
-  //   // Run the cleanUpBeforeUpdate function
-  //   const result = await cleanUpBeforeUpdate(config);
-  //
-  //   const allFiles = searchFilesInDirectory({
-  //     directoryPath: config.projectCatalog,
-  //     excludedFileNames: ['.DS_Store'],
-  //     excludedPhrases: ['.backup'],
-  //   });
-  //
-  //   // Expectations: The files should remain untouched as there were no files to clean up
-  //   expect({ ...result, allFiles }).toStrictEqual({
-  //     config: mockConfig.step.buildFromConfig.empty,
-  //     snpFileMapConfig: mockSnpFileMapConfig.step.buildFromConfig.empty,
-  //     allFiles: [
-  //       'test/mockProject/.gitignore',
-  //       'test/mockProject/.snp/repositoryMap.json',
-  //       'test/mockProject/.snp/snp.config.json',
-  //       'test/mockProject/.snp/templateCatalog/.gitignore-default.md',
-  //       'test/mockProject/.snp/templateCatalog/README.md-default.md',
-  //       'test/mockProject/.snp/templateCatalog/package.json-default.md',
-  //       'test/mockProject/.snp/templateCatalog/tools/test.sh-default.md',
-  //       'test/mockProject/.snp/templateCatalog/tsconfig.json-default.md',
-  //       'test/mockProject/.snp/templateCatalog/yarn.lock-default.md',
-  //       'test/mockProject/.snp/temporary/.gitignore-default.md',
-  //       'test/mockProject/.snp/temporary/README.md-default.md',
-  //       'test/mockProject/.snp/temporary/package.json-default.md',
-  //       'test/mockProject/.snp/temporary/test.sh-default.md',
-  //       'test/mockProject/.snp/temporary/tsconfig.json-default.md',
-  //       'test/mockProject/.snp/temporary/yarn.lock-default.md',
-  //       'test/mockProject/README.md',
-  //       'test/mockProject/package.json',
-  //       'test/mockProject/tools/test.sh',
-  //       'test/mockProject/tsconfig.json',
-  //       'test/mockProject/yarn.lock',
-  //     ],
-  //   });
-  //
-  //   // Ensure that the function returned the correct config object
-  //   expect(result).toStrictEqual(config);
-  // });
+  it('with extra files', async () => {
+    config = { ...mockConfig.step.cleanUp.fullFiled, ...partialConfig };
+    snpFileMapConfig = { ...mockSnpFileMapConfig.step.cleanUp.fullFiled };
 
-  // it('should handle errors gracefully', async () => {
-  //   // Simulate an error by providing an invalid path in the fileMap
-  //   config = { ...mockConfig.step.createConfigFile };
-  //
-  //   await createFile({
-  //     filePath: config.snpConfigFile,
-  //     content: JSON.stringify(config),
-  //   });
-  //   await createFile({
-  //     filePath: config.snpFileMapConfig,
-  //     content: JSON.stringify({
-  //       ...mockSnpFileMapConfig.step.prepareBaseSnpFileMap,
-  //       fileMap: ['invalid/path/to/file'], // Invalid path to simulate an error
-  //     }),
-  //   });
-  //
-  //   await expect(cleanUpBeforeUpdate(config)).rejects.toThrow('Error while downloading config from github');
-  // });
+    // Set up mock files for testing
+    await createFile({
+      filePath: config.snpConfigFile,
+      content: JSON.stringify(config),
+    });
+    await createFile({
+      filePath: config.snpFileMapConfig,
+      content: JSON.stringify(snpFileMapConfig),
+    });
+
+    let keysToCreateFile: NonNullable<unknown>[] = Object.keys(snpFileMapConfig.snpFileMap || {}).slice(0, 3);
+
+    keysToCreateFile = keysToCreateFile.map((key: any) => {
+      if (snpFileMapConfig.snpFileMap) {
+        return snpFileMapConfig.snpFileMap[key];
+      }
+      return [];
+    });
+
+    //Manual creation of custom and extend files
+    const pathToCreateCustomFile = extractAndReplacePaths(keysToCreateFile, '-default.md', '-custom.md');
+    const pathToCreateExtendFile = extractAndReplacePaths(keysToCreateFile, '-default.md', '-extend.md');
+    const pathToCreate = [...pathToCreateExtendFile, ...pathToCreateCustomFile, ...snpFileMapConfig.createdFileMap];
+    const uniquePathToCreate = [...new Set(pathToCreate)];
+
+    for (const file of uniquePathToCreate) {
+      await createFile({
+        filePath: file,
+        content: `{"path": "${file}"}`,
+      });
+    }
+
+    // Run the cleanUpBeforeUpdate function
+    const result = await cleanUpBeforeUpdate(config);
+
+    const allFiles = searchFilesInDirectory({
+      directoryPath: config.projectCatalog,
+      excludedFileNames: ['.DS_Store'],
+      excludedPhrases: ['.backup'],
+    });
+
+    // Expectations: The files should remain untouched as there were no files to clean up
+    expect({ ...result, allFiles }).toStrictEqual({
+      config: mockConfig.step.cleanUpBeforeUpdate.fullFiled,
+      snpFileMapConfig: mockSnpFileMapConfig.step.cleanUpBeforeUpdate.fullFiled,
+      allFiles: [
+        'test/mockProject/.snp/repositoryMap.json',
+        'test/mockProject/.snp/snp.config.json',
+        'test/mockProject/.snp/templateCatalog/.gitignore-custom.md',
+        'test/mockProject/.snp/templateCatalog/.gitignore-extend.md',
+        'test/mockProject/.snp/templateCatalog/README.md-custom.md',
+        'test/mockProject/.snp/templateCatalog/README.md-extend.md',
+        'test/mockProject/.snp/templateCatalog/package.json-custom.md',
+        'test/mockProject/.snp/templateCatalog/package.json-extend.md',
+      ],
+      deletedPath: [
+        './test/mockProject/.snp/templateCatalog/.gitignore-default.md',
+        './test/mockProject/.gitignore',
+        './test/mockProject/.snp/templateCatalog/README.md-default.md',
+        './test/mockProject/README.md',
+        './test/mockProject/.snp/templateCatalog/package.json-default.md',
+        './test/mockProject/package.json',
+        './test/mockProject/.snp/templateCatalog/tools/test.sh-default.md',
+        './test/mockProject/tools/test.sh',
+        './test/mockProject/.snp/templateCatalog/tsconfig.json-default.md',
+        './test/mockProject/tsconfig.json',
+        './test/mockProject/.snp/templateCatalog/yarn.lock-default.md',
+        './test/mockProject/yarn.lock',
+      ],
+    });
+  });
 });
