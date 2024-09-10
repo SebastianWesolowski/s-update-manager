e99df760eac8cc84aaf43143bc50353d7d8b962a
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-09-11 01:15:07 +0200

    feat: ✨ [SC-24] new url for prefix

--- .releaserc.js
+++ .releaserc.js
@@ -27,7 +27,6 @@ module.exports = {
       '@semantic-release/release-notes-generator',
       {
         preset: 'conventionalcommits',
-
         presetConfig: {
           types: [
             {
